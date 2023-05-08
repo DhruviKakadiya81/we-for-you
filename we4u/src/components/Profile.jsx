946842e2ab5db9221c5efedfa80be3326137dc2c
Dhruvi <dhruvikakadiya41@gmail.com>
2023-05-08 13:41:30 +0530

    profile_done

--- we4u/src/components/Profile.jsx
+++ we4u/src/components/Profile.jsx
@@ -3,7 +3,8 @@ import { Navbar } from './Navbar'
 import { FormControl, FormGroup, Input, InputLabel, Typography, Button,FormLabel,RadioGroup,FormControlLabel,Radio} from '@mui/material';
 import "../css/Profile.css";
 import { makeStyles,TextField } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import {Modal} from 'react-bootstrap';
+import { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -21,55 +22,79 @@ const Profile = () => {
   const classes = useStyles();
   return (
     <>
- <Navbar/>
- <FormGroup className='mx-auto main_container'>
-                    <Typography variant="h4" className='Pro_heading'>Profile</Typography>
-                 
-                    <div className='mt-5 mx-auto'>
-                                <FormControl className='mb-3'>
-                                    <InputLabel className='mx-3'>Enter First Name</InputLabel>
-                                    <Input type="text" name="name" className='mx-3 my-3'  />
-                                    </FormControl><br/>
-                                    <FormControl className='mb-3'>
-                                    <InputLabel className='mx-3' >Enter Second Name</InputLabel>
-                                    <Input type="text" name="name" className='mx-3 my-3'  />
-                                    </FormControl><br/>
-                                    <FormControl className={classes.container}>
-                                    <TextField
-                                        id="date"
-                                        label="Birthday"
-                                        type="date"
-                                        defaultValue="2023-01-01"
-                                        className={classes.textField}
-                                        InputLabelProps={{
-                                          shrink: true,
-                                        }}
-                                      />
-                                    </FormControl><br/>
-                                    <FormControl>
-                                    <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
-                                    <RadioGroup
-                                      row
-                                      aria-labelledby="demo-row-radio-buttons-group-label"
-                                      name="row-radio-buttons-group"
-                                      defaultValue="female"
-                                    >
-                                      <FormControlLabel value="female" control={<Radio />} label="Female" />
-                                      <FormControlLabel value="male" control={<Radio />} label="Male" />
-                                      <FormControlLabel value="other" control={<Radio />} label="Other" />
-                                    </RadioGroup>
-                                  </FormControl><br/>
-                                  <FormControl className='mb-3 mx-auto'>
-                                  <Button type='submit' variant='contained' className='my-3 px-5 py-3 '>Add Agent</Button>
-                                </FormControl><br/>
-                                <FormControl>
-                                  <Link to="#">Change Password?</Link>
-                                </FormControl>
-                    </div>
-                  
-      </FormGroup>
+    <Navbar/>
+    <FormGroup>
+      <Typography>Profile</Typography>
+       <div>
+        <FormControl>
+          <InputLabel>Enter First Name</InputLabel>
+          <Input type='text'/>
+        </FormControl>
+        <FormControl>
+          <InputLabel>Enter Second Name</InputLabel>
+          <Input type='text'/>
+        </FormControl>
+        <FormControl>
+          <TextField 
+            id='date'
+            label='Birthday'
+          />
+        </FormControl>
+       </div>
+    </FormGroup>
     </>
   )
 }
 
 export default Profile
+
+const ChangePass = (props)=>{
+  const [isshow, invokemodel] = useState(false);
+  
+  const initmodel = () => {
+    return invokemodel(!isshow);
+  }
+  const handledelete = async(id,e) =>{
+   }
+  return(
+    <>
+     <Button variant="contained" style={{backgroundColor:"white",color:"black"}} onClick={initmodel}>
+       Change Password??
+      </Button>
+      <Modal show={isshow} style={{overflowX:"scroll",width:"100%"}} >
+        <Modal.Header closeButton onClick={initmodel}>
+          <Modal.Title className='' > 
+            Change Password
+          </Modal.Title>
+        </Modal.Header>
+      
+          <Modal.Body>
+              <div className="dlt">
+              <FormControl className='mb-3 detail_container'>
+                  <InputLabel className='mx-3'>Old Password</InputLabel>
+                  <Input type="text" name="name" className='my-3'  />
+              </FormControl><br/>
+              <FormControl className='mb-3 detail_container'>
+                  <InputLabel className='mx-3' >New Password</InputLabel>
+                  <Input type="text" name="name" className='my-3'  />
+              </FormControl><br/>
+              <FormControl className='mb-3 detail_container'>
+                  <InputLabel className='mx-3' >Confirm New Password</InputLabel>
+                  <Input type="text" name="name" className='my-3'  />
+              </FormControl><br/>
+              </div>             
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="" className="mx-3" onClick={initmodel} style={{backgroundColor:"red"}}>
+              CLOSE
+            </Button>
+            <Button variant=""  className="mx-3" type='submit' style={{backgroundColor:"black",color:"white"}} onClick={(e)=>handledelete(props.id,e)}>
+              Change
+            </Button>
+          </Modal.Footer>
+      
+      </Modal>
+     
+    </>
+  )
+}
