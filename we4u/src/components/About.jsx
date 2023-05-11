5030c3c99c7aab05cfeadad3afbf12229c43b2f4
Mansi <mansirathod763@gmail.com>
2023-05-11 13:27:54 +0530

    about

--- we4u/src/components/About.jsx
+++ we4u/src/components/About.jsx
@@ -59,8 +59,10 @@ const options = {
   return (
     <>
     <Navbar/>
-<section className="page1 mt-5" >
-<div className='div1 w-75 mx-auto p-5' style={{width:"900px"}}>
+
+{/* <div className='div1 mx-auto p-5 ' style={{width:"95%" ,backgroundColor:"rgb(245,245,245)"}}>
+
+<div style={{}}>
 <h2>Who We Are</h2>
 <br />
 <p>We4U is a platform that provides a range of home services, including beauty and wellness services, cleaning services, pest control, plumbing, carpentry, electrical, appliance repair, and more. It connects customers with local service providers who are vetted and trained to provide high-quality services at affordable prices.
@@ -68,6 +70,8 @@ const options = {
 We4U has revolutionized the home services industry by providing a convenient and reliable platform for booking and managing home services. It offers transparent pricing, easy online booking, and a hassle-free experience for both customers and service providers.
 
 </p>
+</div>
+
 
 
 <div class="d-flex justify-content-center flex-wrap " id="grid1">
@@ -109,27 +113,16 @@ We4U has a rigorous vetting process for its service providers to ensure that the
 <br /><br />
 <h2>Our Members</h2>
 
-{/* <TestiMonials/> */}
+
 <div className="test" >
   <div id="testimonial" className="testimonials" >
-  {/* <div className="container "> */}
+ 
   <div className="row">
   <div className="col-md-12">
                         <OwlCarousel id="customer-testimonoals" className="owl-carousel owl-theme" {...options}>
                             {
                                 testiMonials.length === 0 ?
-                                    {/* <div class="item">
-                                        <div class="shadow-effect">
-                                            <img class="img-circle" src='images/nails.jpg' />
-
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
-                                        </div>
-                                        <
-                                        div class="testimonial-name">
-                                            <h5>Rajon Rony</h5>
-                                            <small>ITALY</small>
-                                        </>
-                                    </div> : */}:
+                                    {}:
                                     testiMonials.map(testiMonialDetail => {
                                         return (
                                             <TestiMonialsDetails testiMonialDetail={testiMonialDetail} key={testiMonialDetail._key} />
@@ -139,18 +132,135 @@ We4U has a rigorous vetting process for its service providers to ensure that the
                             }
                         </OwlCarousel>
                     </div>
-                    {/* </div> */}
+                 
   </div>
   </div>
 </div>
 
+</div> */}
+<div class="d-flex justify-content-center flex-wrap mt-5 ">
+ 
+
+      <img className='img_div' src="images/electrician.jpg" style={{borderRadius:"100%" , border:"4px solid gray" ,  boxShadow:  "rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset"}}  height="500" width="500" alt="" srcset="" />
+   
+    <div class="col-7 txt_div" style={{textAlign:"left"}}>
+    <h1 className='text-center mt-5'>WE4U</h1>
+    <p className='p-1 text-left mx-5' style={{fontSize:"22px"}}>We4U is a platform that provides a range of home services, including beauty and wellness services, cleaning services, pest control, plumbing, carpentry, electrical, appliance repair, and more. It connects customers with local service providers who are vetted and trained to provide high-quality services at affordable prices.
+
+     We4U has revolutionized the home services industry by providing a convenient and reliable platform for booking and managing home services. It offers transparent pricing, easy online booking, and a hassle-free experience for both customers and service providers.
+
+</p>
+      </div>
+    </div>
+
+    <h1 className='text-center mt-5'>==: Our Members :==</h1>
+
+<div className="test" >
+
+  <div id="testimonial" className="testimonials" >
+ 
+  <div className="row">
+  <div className="col-md-12">
+                        <OwlCarousel id="customer-testimonoals" className="owl-carousel owl-theme" {...options}>
+                            {
+                                testiMonials.length === 0 ?
+                                    {}:
+                                    testiMonials.map(testiMonialDetail => {
+                                        return (
+                                            <TestiMonialsDetails testiMonialDetail={testiMonialDetail} key={testiMonialDetail._key} />
+
+                                        )
+                                    })
+                            }
+                        </OwlCarousel>
+                    </div>
+                 
+  </div>
+  </div>
 </div>
+    
+    <h1 className='text-center' style={{marginTop:"50px"}}>==: About Us :==</h1>
+    
+    <div class="d-flex justify-content-center flex-wrap " id="grid1">
+  
+{/* <div class="row mt-3">
+    <div class="col mx-1 my-1 " >
+   <div  class ="center_text"> 
+    <h2>45000+</h2>
+    <p>Trained Proffessor</p>
+    </div>
+    </div>
+    <div class="col mx-1 my-1">
+      <div class ="center_text" >
+      <h2>10 Million+</h2>
+      <p>Happy Customers</p>
+    </div>
+    </div>
+    <div class="col mx-1 my-1">
+     <div class ="center_text">
+     <h2>63</h2>
+     <p>cities</p>
+    </div>
+    </div>
+    <div class="col mx-1 my-1">
+     <div class ="center_text">
+     <h2>5</h2>
+     <p> Countries</p>
+     </div>
+    </div>
+  </div> */}
+  <div  class ="center_text"> 
+  <div class="cen_text" >
+    <h2>45000+</h2>
+    <p>Trained Proffessor</p>
+    </div>
+    </div>
+
+    <div   class ="center_text"> 
+    <div class="cen_text">
+    <h2>45000+</h2>
+    <p>Trained Proffessor</p>
+    </div>
+    </div>
+
+    <div  class ="center_text"> 
+    <div class="cen_text">
+
+    <h2>45000+</h2>
+    <p>Trained Proffessor</p>
+    </div>
+    </div>
+
+    <div  class ="center_text"> 
+    <div class="cen_text">
+    
+    <h2>45000+</h2>
+    <p>Trained Proffessor</p>
+    </div>
+    </div>
+
+</div>
+
+
+
+<h1 className='text-center' style={{marginTop:"70px"}}>==: How We Do It :==</h1>
+
+<div className="text-center mt-5 mx-auto w-75 p-5" style={{boxShadow:  "rgb(105,105,105) 0px 12px 28px 0px, rgb(105,105,105) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset"}}>
+<p>We4U provide customer-centric approach, where it ensures a seamless experience for customers. Customers can also rate and review the service providers, which helps maintain service quality and build trust among customers.
+<br /><br />
+We4U has a rigorous vetting process for its service providers to ensure that they meet its quality standards. Service providers have to go through background checks, training, and assessments before they can provide services on the platform. This helps maintain service quality and ensures that customers receive the best possible service.
+</p>
+</div>
+
+<br /><br />
+
+
 
 
 
 
       
-    </section>
+
     
     <Footer/>
    
