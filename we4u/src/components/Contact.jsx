7b491088955cf0c53a36a65e9363c841ae29361a
Dhruvi <dhruvikakadiya41@gmail.com>
2023-05-16 19:50:39 +0530

    stepper_2

--- we4u/src/components/Contact.jsx
+++ we4u/src/components/Contact.jsx
@@ -11,6 +11,7 @@ import {
 } from "react-icons/fa";
 import {TextField,Button} from "@mui/material";
 import { Link } from 'react-router-dom';
+import {Footer} from './Footer'
 
 export const Contact = () => {
     
@@ -18,65 +19,124 @@ export const Contact = () => {
     
     <>
     <Navbar/>
- 
-     <div className="container" style={{zIndex:"-1"}}>
-        <div className="form">
-            <div className="contact-info order-2 order-lg-1">
-                <h3 className="title">Let's Contact With Us</h3>
-                <p className="text">
-                    We Love to Give Service Who Love Us.
-                </p>
-
-                <div className="info">
-                    <div className="information">
-                        <FaEnvelope className='icon'/>
-                        <p className='info_detail'>we4uservices3@gmail.com</p>
-                    </div>
-                    <div className="information">
-                        <FaPhoneAlt className='icon'/>
-                        <p className='info_detail'>9035627167</p>
-                    </div>
-                </div>
-
-                <div className="social-media">
-                    <p>Connect With Us : </p>
-                    <div className="social-icons">
-                        <Link to="#">
-                            <FaFacebook className='sub_social_icon'/>
-                        </Link>
-                        <Link to="#">
-                            <FaTwitter className='sub_social_icon'/>
-                        </Link>
-                        <Link to="#">
-                            <FaInstagram className='sub_social_icon'/>
-                        </Link>
-                        <Link to="#">
-                            <FaLinkedin className='sub_social_icon'/>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-
-             <div className="contact-form order-1 order-lg-2 p-2">
-            <form action="#" autoComplete='off' className='cont_form'>
-            <h3 className="title mb-5">Contact Us</h3>
-            <div className="input-container">
-            <TextField id="outlined-basic" label="UserName" variant="outlined" className='input' style={{width:"90%"}}/>
-            </div>
-            <div className="input-container">
-            <TextField id="outlined-basic" label="Email" variant="outlined" className='input' style={{width:"90%"}}/>
-            </div>
-            <div className="input-container">
-            <TextField id="outlined-basic" label="Phone" variant="outlined" className='input' style={{width:"90%"}}/>
-            </div>
-            <div className="input-container">
-            <TextField id="outlined-textarea" label="Message" multiline className='input' style={{width:"90%"}}/>
-            </div>
-            <Button variant="contained" className='btn'>Send</Button>
-            </form>
-            </div>
+    <div class="c_container2 hero-image" style={{background:`url(Images/cont_first.png)`,backgroundSize:"cover"}}>
+        <div class="hero-text c_p_container">
+          <p class="c_par1">Contact Us</p>
+          <p class="c_par2">We're easy to get in touch with</p>
         </div>
-     </div>
+      </div>
+      <div class="c_container3">
+      </div>
+      <div class="c_container4 hero-text">
+        <div class="container text-center">
+          <div class="row">
+            <div class="col c_detail_container">
+              <p class="main_head">
+                Get Into Touch
+              </p>
+              <hr/>
+              <div class="c_details">
+                <p class="c_add_head">
+                  Address:
+                </p>
+                <p class="c_add_detail1">
+                  58, Ralph Ave
+                </p>
+                <p class="c_add_detail2">
+                  New York,New York 1111
+                </p>
+                <p class="c_phone_continer">
+                  <span class="c_phone_head">Phone:</span><span class="c_phone_detail"> +1 800 000 111</span>
+                </p>
+                <p class="c_email_container">
+                  <span class="c_email_head">Email:</span><span class="c_email_detail"> Contact@Example.com</span>
+                </p>
+              </div>              
+            </div>
+            <div class="col c_form_container">
+              <div class="d-flex justify-content-center">
+                <input type="text" name="firstName" class="c_input_first_name mx-2 my-2" placeholder="Enter First Name"/>
+                <input type="text" name="Last name" class="c_input_last_name mx-2 my-2" placeholder="Enter Last Name"/>
+              </div>
+              <div class="d-flex justify-content-center">
+                <input type="number" name="phoneNumber" class="c_input_phone_number mx-2 my-2" placeholder="Phone Number"/>
+                <input type="Email" name="Email" class="c_input_email_address mx-2 my-2" placeholder="Email Address"/>
+              </div>
+               <textarea name="message" class="c_input_message mx-2 my-2" placeholder="Your Message"></textarea><br/>
+               <button type="submit" class="c_send_btn">Send Message</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="c_container5 hero-text">
+        <div class="c_inner_container">
+          <p class="c_loc_header">
+            Location Of Restaurants All Over The India
+          </p>
+          <hr class="c_hr_c5"/>
+          <div class="container text-center">
+            <div class="d-flex justify-content-evenly c_loc_outer">
+              <div class="c_loc_inner my-2">
+                <img src="/images/Vector.png" alt="" height="40px"/>
+                <p class="c_loc_title">Surat</p>
+                <p class="c_loc_desc">58,Ralph Ave New York,New York 111</p>
+              </div>
+              <div class="c_loc_inner my-2">
+                <img src="/images/Vector.png" alt="" height="40px"/>
+                <p class="c_loc_title">Amdavad</p>
+                <p class="c_loc_desc">58,Ralph Ave New York,New York 111</p>
+               </div> 
+               <div class="c_loc_inner my-2">
+                <img src="/images/Vector.png" alt="" height="40px"/>
+                <p class="c_loc_title">Vadodara</p>
+                <p class="c_loc_desc">58,Ralph Ave New York,New York 111</p>
+              </div>
+            </div>
+          </div>
+          <div class="c_second_header">
+            <p class="c_second_heading">Upcoming</p>
+            <div class="container text-center">
+              <div class="d-flex justify-content-evenly c_sec_loc_outer">
+                <div class="c_sec_loc_inner my-2">
+                  <img src="/images/Vector.png" alt="" height="40px"/>
+                  <p class="c_loc_sec_title">Indore</p>
+                  <p class="c_loc_sec_desc">58,Ralph Ave New York,New York 111</p>
+                </div>
+                <div class="c_sec_loc_inner my-2">
+                  <img src="/images/Vector.png" alt="" height="40px"/>
+                  <p class="c_loc_sec_title">Bengaluru</p>
+                  <p class="c_loc_sec_desc">58,Ralph Ave New York,New York 111</p>
+                </div>
+                <div class="c_sec_loc_inner my-2">
+                  <img src="/images/Vector.png" alt="" height="40px"/>
+                  <p class="c_loc_sec_title">Chennai</p>
+                  <p class="c_loc_sec_desc">58,Ralph Ave New York,New York 111</p>
+                </div>
+                <div class="c_sec_loc_inner my-2">
+                  <img src="/images/Vector.png" alt="" height="40px"/>
+                  <p class="c_loc_sec_title">Jamshedpur</p>
+                  <p class="c_loc_sec_desc">58,Ralph Ave New York,New York 111</p>
+                </div>
+                <div class="c_sec_loc_inner my-2">
+                  <img src="/images/Vector.png" alt="" height="40px"/>
+                  <p class="c_loc_sec_title">Patna</p>
+                  <p class="c_loc_sec_desc">58,Ralph Ave New York,New York 111</p>
+                </div>
+                <div class="c_sec_loc_inner my-2">
+                  <img src="/images/Vector.png" alt="" height="40px"/>
+                  <h5 class="c_loc_sec_title">Patna</h5>
+                  <p class="c_loc_sec_desc">58,Ralph Ave New York,New York 111</p>
+                </div>
+              </div>
+            </div>
+          </div>   
+      </div>
+      
+      <div class="map_container">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7441.68091850823!2d72.78242768220215!3d21.15874651462442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1675149979922!5m2!1sen!2sin" width="1600" height="460" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="c_map"></iframe>
+      </div>
+      </div>
+      <Footer/>
     </>
   )
 }
