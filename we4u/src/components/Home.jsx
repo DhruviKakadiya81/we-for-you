ca5377f99558e8b55e351fbf279d006e45b02546
Krupa <mavanikrupa252@gmail.com>
2023-05-18 09:50:48 +0530

    feedback

--- we4u/src/components/Home.jsx
+++ we4u/src/components/Home.jsx
@@ -5,18 +5,11 @@ import '../css/Home.css';
 import showservice from '../services/Services';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import '../css/About.css'
-import ReactPlayer from 'react-player'
+import '../css/About.css';
+import OwlCarousel from 'react-owl-carousel2';
+import 'react-owl-carousel2/lib/styles.css';
+
 export const Home = () => {
-  const [service, setService] = useState([]);
-  var ser;
-  const fetchSer = async (event) => {
-    // event.prventDefault();
-    ser = await showservice.getservice();
-    console.log("data :", ser.data.data[0].s_name);
-    setService(ser);
-    // console.log("data2 :", service.data.data.length);
-  }
   function reveal() {
     var reveals = document.querySelectorAll(".reveal");
 
@@ -34,6 +27,44 @@ export const Home = () => {
   }
 
   window.addEventListener("scroll", reveal);
+  const options = {
+    loop: true,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  };
+
+  const handleNextClick = () => {
+    owl.current.next();
+  };
+
+  const handlePrevClick = () => {
+    owl.current.prev();
+  };
+
+  const owl = React.useRef(null);
+
+  const [service, setService] = useState([]);
+  var ser;
+  const fetchSer = async (event) => {
+    // event.prventDefault();
+    ser = await showservice.getservice();
+    console.log("data :", ser.data.data[0].s_name);
+    setService(ser);
+    // console.log("data2 :", service.data.data.length);
+  }
+  
+
 
 
   useEffect((event) => {
@@ -45,18 +76,16 @@ export const Home = () => {
       <Navbar />
       <section>
         <div className="container-fluid p-0">
-       
+
           <div className="s01" style={{
             background: `url("images/homeimag9.jpg")`, backgroundSize: "cover",
             backgroundPosition: "center center", width: "auto"
           }}>
-            <form className=" reveal fade-In">
+            <form className="reveal fade-In">
               <fieldset className="inner-form mb-0 pb-0" style={{ fontSize: "30px", width: "90%", margin: "auto" }}>
                 <legend className="pb-0 mb-0" style={{ fontSize: "40px", width: "90%" }}> Discover the Amazing Services</legend>
               </fieldset>
               <div className="inner-form" style={{ width: "90%", margin: "auto" }}>
-
-
                 <div className="input-field first-wrap">
                   <input id="search" type="text" placeholder="What are you looking for?" />
                 </div>
@@ -77,10 +106,10 @@ export const Home = () => {
 
       <section className="services1">
         <h1 className='serviceh1'>  Our Services </h1>
-         <div className='services_scroll'>
+        <div className='services_scroll'>
           {service.data != undefined && service.data.data.length > 0 && (
             <section className="services">
-              <div class="d-flex flex-wrap justify-content-center">
+              <div className="d-flex flex-wrap justify-content-center">
                 {service.data.data.map(product => (
                   <div className='row m-2'>
                     <div className="card" style={{ width: "10rem", boxShadow: "rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset" }} >
@@ -100,58 +129,58 @@ export const Home = () => {
 
 
 
-      <section class="progress-section">
-        <div class="container">
-            <div class="row mb-4 justify-content-center">
-                <div class="col-lg-7 col-md-12 col-sm-12">
-                    <div class="process-heading text-center mb-5 ">
-                        <span class="gradient-text">How to Get Started</span>
-                        <h2 class="mb-3 mt-3">Fast & Easy Application Process.</h2>
-                        <p>Transparent and fair costs, with no hidden fees</p>
-                    </div>
-                </div>
+      <section className="progress-section">
+        <div className="container">
+          <div className="row mb-4 justify-content-center">
+            <div className="col-lg-7 col-md-12 col-sm-12">
+              <div className="process-heading text-center mb-5 ">
+                <span className="gradient-text">How to Get Started</span>
+                <h2 className="mb-3 mt-3">Fast & Easy Application Process.</h2>
+                <p>Transparent and fair costs, with no hidden fees</p>
+              </div>
             </div>
-            <div class="row justify-content-center">
-                <div class="progress-img">
-                    <img className="w-100" src="images/get-started.png"  alt="progress"/>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-10">
-                    <div class="progress-card">
-                        <div class="progress-card-body text-center">
-                            <img src="/images/apply.png" alt="apply" width="80px"/>
-                            <h5 class="card-title mt-3 mb-3">Apply In 10 Minutes</h5>
-                            <p class="p mt-2 mb-5">Submission of Online document with Basic details Docs can be
-                                submitted via
-                                Email or Whatsapp as well</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-10">
-                    <div class="progress-card">
-                        <div class="progress-card-body text-center">
-                            <img src="images/disition.png" alt="disition" width="80px"/>
-                            <h5 class="card-title mt-3 mb-3">A Decision In 24 Hours</h5>
-                            <p class="p mt-2 mb-5">Get a call and Instant quotes(Rate / Loan amount ) with options of
-                                banks</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-10">
-                    <div class="progress-card">
-                        <div class="progress-card-body text-center">
-                            <img src="images/funded.png" alt="funded" width="80px"/>
-                            <h5 class="card-title mt-3 mb-3">Your Loan Is Funded</h5>
-                            <p class="p mt-2 mb-5">Submission of docs with bank or Finance company for further process
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="text-center mt-3">
-                    <button class="btn btn-primary">Apply Now</button>
-                </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="progress-img">
+              <img className="w-100" src="images/get-started.png" alt="progress" />
             </div>
+            <div className="col-lg-3 col-md-4 col-sm-10">
+              <div className="progress-card">
+                <div className="progress-card-body text-center">
+                  <img src="/images/apply.png" alt="apply" width="80px" />
+                  <h5 className="card-title mt-3 mb-3">Apply In 10 Minutes</h5>
+                  <p className="p mt-2 mb-5">Submission of Online document with Basic details Docs can be
+                    submitted via
+                    Email or Whatsapp as well</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-sm-10">
+              <div className="progress-card">
+                <div className="progress-card-body text-center">
+                  <img src="images/disition.png" alt="disition" width="80px" />
+                  <h5 className="card-title mt-3 mb-3">A Decision In 24 Hours</h5>
+                  <p className="p mt-2 mb-5">Get a call and Instant quotes(Rate / Loan amount ) with options of
+                    banks</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-sm-10">
+              <div className="progress-card">
+                <div className="progress-card-body text-center">
+                  <img src="images/funded.png" alt="funded" width="80px" />
+                  <h5 className="card-title mt-3 mb-3">Your Loan Is Funded</h5>
+                  <p className="p mt-2 mb-5">Submission of docs with bank or Finance company for further process
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="text-center mt-3">
+              <button className="btn btn-primary">Apply Now</button>
+            </div>
+          </div>
         </div>
-    </section>
+      </section>
 
 
 
@@ -160,33 +189,119 @@ export const Home = () => {
 
 
 
-      <section className='beauty_saloon pt-3 pb-3'>
+      {/* <section className='beauty_saloon pt-3 pb-3'>
         <h1 className='beauty_saloon_title'>Beauty Saloon</h1>
         <div className="d-flex flex-wrap justify-content-center">
+
           <div className="card mx-4 my-4">
-            <img src="Images/manicure.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Manicure</h5>
-              <p class="card-text">A Manicure usually consists of filing and shaping the free edge of nails, pushing and clipping.</p>
+            <img src="Images/manicure.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Manicure</h5>
+              <p className="card-text">A Manicure usually consists of filing and shaping the free edge of nails, pushing and clipping.</p>
             </div>
           </div>
           <div className="card mx-4 my-4">
-            <img src="Images/keratin.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Keratin</h5>
-              <p class="card-text">A keratin treatment is a process that smooths and often straightens hair.</p>
+            <img src="Images/keratin.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Keratin</h5>
+              <p className="card-text">A keratin treatment is a process that smooths and often straightens hair.</p>
             </div>
           </div>
           <div className="card mx-4 my-4">
-            <img src="Images/hair_cutting.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Hair Cutting</h5>
-              <p class="card-text">We Are Doing Many Types Of Hair Cutting like layered, one-length, Face-Look.</p>
+            <img src="Images/hair_cutting.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Hair Cutting</h5>
+              <p className="card-text">We Are Doing Many Types Of Hair Cutting like layered, one-length, Face-Look.</p>
             </div>
           </div>
         </div>
 
+      </section> */}
+
+
+
+      <section className="feedback-section">
+        <div className="container">
+
+          <div className="row mb-4 justify-content-center">
+            <div className="col-lg-7 col-md-12 col-sm-12">
+              <div className="process-heading text-center mb-5 ">
+                <span className="gradient-text">Feedback</span>
+                <h2 className="mb-3 mt-3">What they’re talking about
+                  our company</h2>
+              </div>
+            </div>
+          </div>
+
+          
+            <div className="row d-flex flex-wrap justify-content-center" id="feedback-carousel">
+           
+              <OwlCarousel ref={owl} options={options}>
+                <div className="item">
+                  <div className="card mx-2 my-4">
+                    <img src="Images/manicure.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title text-center">Manicure</h5>
+                      <p className="card-text text-justify">A Manicure usually consists of filing and shaping the free edge of nails, pushing and clipping.</p>
+                    </div>
+                    <div className="star-rating text-center">
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span>5.0</span>
+                    </div>
+                  </div>
+
+                </div>
+                <div className="item">
+                  <div className="card mx-2 my-4">
+                    <img src="Images/keratin.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title text-center">Keratin</h5>
+                      <p className="card-text text-justify">A keratin treatment is a process that smooths and often straightens hair.</p>
+                    </div>
+                    <div className="star-rating text-center">
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <div>5.0</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="item">
+                  <div className="card mx-2 my-4">
+                    <img src="Images/hair_cutting.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title text-center">Hair Cutting</h5>
+                      <p className="card-text text-justify">We Are Doing Many Types Of Hair Cutting like layered, one-length, Face-Look.</p>
+                    </div>
+                    <div className="star-rating text-center">
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span><i className="bi bi-star-fill" style={{ color: "#ffff00" }}></i></span>
+                      <span>5.0</span>
+                    </div>
+                  </div>
+                </div>
+              </OwlCarousel>
+            </div>
+            <div className="owl-carousel__nav">
+              <button className="owl-carousel__next" onClick={handleNextClick}>Next</button>
+              <button className="owl-carousel__prev" onClick={handlePrevClick}>Prev</button>
+            </div>
+          </div>
+
+        
       </section>
+
+
+
 
       <Footer />
 
