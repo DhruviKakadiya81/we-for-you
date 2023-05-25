8ad78502c759117ec1de491f1385fe60affc688e
Krupa <mavanikrupa252@gmail.com>
2023-05-25 11:53:58 +0530

    sp side updated

--- we4u/src/components/Sp.jsx
+++ we4u/src/components/Sp.jsx
@@ -7,29 +7,30 @@ export const Sp = () => {
   return (
     <>
     <Ser_Pro_Navbar/>
-     <section className='ser_home_sec'  style={{background:`url(Images/homeimg9.jpg)`,backgroundSize:"cover",backgroundPosition:"center center"}}>
-      <div className="ser_title_cont mx-5">
-      <p className='earn_title'>Earn More With Us.</p>
-      <p className='grow_title'>Grow Your Business.</p>
-      <p className='detail_title'>Join We4U And Get Partner across India, USA, Singapore And Many More</p>
+     <section className='ser_home_sec'  style={{ backgroundImage: ` linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url("images/sphome.jpg")`,backgroundSize:"contain", height:"100vh",width:"auto",backgroundPosition:"center",backgroundRepeat:"no-repeat",position:"relative",zIndex:"0"}}>
+       <div className="container mx-0 pt-5 mt-5 pl-0">
+       <div className="ser_title_cont mt-5  pt-5 ms-lg-5 pl-ld-5">
+      <p className='earn_title d-flex align-items-center mt-5 pt-md-5 pt-lg-5 pt-sm-0' >Earn More With Us.</p>
+      <p className='grow_title d-flex align-items-center'>Grow Your Business.</p>
+      <p className='detail_title d-flex align-items-center text-dark'>Join We4U And Get Partner across India, USA, Singapore And Many More</p>
+       </div>
+     
       </div>
      </section>
      <div className="parallax mb-0" style={ {
-      /* The image used */
-     backgroundImage: ` linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("images/homeimg2.jpg")`,
-  
-     /* Set a specific height */
+     backgroundImage: ` linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url("images/sphome.jpg")`,
       minHeight: "60vh",
-
-      /* Create the parallax scrolling effect */
       backgroundAttachment: "fixed",
       backgroundPosition: "center",
       backgroundRepeat: "no-repeat",
       backgroundSize: "cover",
     }}>
-    <p className='ser_home_card_title'>Join We4U And Make Your Business Wide</p>
+      
+      <p className='ser_home_card_title'>Join We4U And Make Your Business Wide</p>
     <p className='ser_home_card_title2'>We4U is an app-based marketplace that empowers profssional like you to become your own buasiness</p>
-     <div className="d-flex justify-content-center">
+    
+   
+     <div className="d-flex  flex-wrap justify-content-center">
      <div className="card mx-4 my-5" style={{width:"16rem",background:`url(Images/ser_home_back.jpg)`,backgroundSize:"cover"}} >
       {/* <img src="..." className="card-img-top" alt="..."/> */}
       <div className="card-body">
@@ -52,8 +53,9 @@ export const Sp = () => {
       </div>
     </div>
      </div>
+     <Footer/>
     </div>
-    <Footer/>
+  
     </>
    
   )
