4130aa7a8e7d17a7f95685b00788df652d2bfda6
Krupa <mavanikrupa252@gmail.com>
2023-05-29 17:29:36 +0530

    subser design changed

--- we4u/src/components/Sp.jsx
+++ we4u/src/components/Sp.jsx
@@ -6,57 +6,74 @@ import "../css/sp.css"
 export const Sp = () => {
   return (
     <>
-    <Ser_Pro_Navbar/>
-     <section className='ser_home_sec'  style={{ backgroundImage: ` linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url("images/sphome.jpg")`,backgroundSize:"contain", height:"100vh",width:"auto",backgroundPosition:"center",backgroundRepeat:"no-repeat",position:"relative",zIndex:"0"}}>
-       <div className="container mx-0 pt-5 mt-5 pl-0">
-       <div className="ser_title_cont mt-5  pt-5 ms-lg-5 pl-ld-5">
-      <p className='earn_title d-flex align-items-center mt-5 pt-md-5 pt-lg-5 pt-sm-0' >Earn More With Us.</p>
-      <p className='grow_title d-flex align-items-center'>Grow Your Business.</p>
-      <p className='detail_title d-flex align-items-center text-dark'>Join We4U And Get Partner across India, USA, Singapore And Many More</p>
-       </div>
-     
+      <Ser_Pro_Navbar />
+      <section className='ser_home_sec' style={{ backgroundImage: ``, backgroundSize: "", height: "auto", width: "100%", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+        <div className="container pt-5 mt-4">
+          <div className="row d-flex flex-wrap justify-content-lg-evenly">
+            <div className="col-lg-6 col-md-8 col-sm-12 col-xs-12 seca ser_title_cont mt-5 pt-5 pb-5 mb-5">
+              <p className='earn_title' >Earn More With Us.</p>
+              <p className='grow_title'>Grow Your Business.</p>
+              <p className='detail_title text-dark'>Join We4U And Get Partner across India, USA, Singapore And Many More</p>
+            </div>
+            <div className="col-lg-6 col-md-4 d-lg-block pb-5 mb-5 homeimgdiv" >
+              <img src="images/back2.png" className='sideimg' alt="" />
+            </div>
+          </div>
+        </div>
+
+
+      </section >
+
+      <div className="text-center d-flex justify-content-center">
+        <div className="upperdiv w-75 justify-content-center p-3" >
+          <p className='ser_home_card_title'>Join We4U And Make Your Business Wide</p>
+          <p className='ser_home_card_title2'>We4U is an app-based marketplace that empowers profssional like you to become your own buasiness</p>
+        </div>
       </div>
-     </section>
-     <div className="parallax mb-0" style={ {
-     backgroundImage: ` linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url("images/sphome.jpg")`,
-      minHeight: "60vh",
-      backgroundAttachment: "fixed",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-    }}>
-      
-      <p className='ser_home_card_title'>Join We4U And Make Your Business Wide</p>
-    <p className='ser_home_card_title2'>We4U is an app-based marketplace that empowers profssional like you to become your own buasiness</p>
-    
-   
-     <div className="d-flex  flex-wrap justify-content-center">
-     <div className="card mx-4 my-5" style={{width:"16rem",background:`url(Images/ser_home_back.jpg)`,backgroundSize:"cover"}} >
-      {/* <img src="..." className="card-img-top" alt="..."/> */}
-      <div className="card-body">
-        {/* <h5 className="card-title">Card title</h5> */}
-        <p className="card-text ser_home_card_content">4000+</p>
+
+      <div className="parallax mb-0 secondsec" style={{
+        backgroundImage: `url("images/sphome.jpg")`,
+        backgroundAttachment: "fixed",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        height: "100%"
+
+
+
+      }}>
+
+        <section className='section2'>
+          <div className="d-flex  flex-wrap justify-content-center pt-5">
+            <div className="card mx-4 my-5" style={{ width: "16rem", background: `url(images/ser_home_back.jpg)`, backgroundSize: "cover" }}>
+              {/* <img src="..." className="card-img-top" alt="..."/> */}
+              <div className="card-body">
+                {/* <h5 className="card-title">Card title</h5> */}
+                <p className="card-text ser_home_card_content">4000+</p>
+              </div>
+            </div>
+            <div className="card mx-4 my-5" style={{ width: "16rem", background: `url(Images/ser_home_back.jpg)`, backgroundSize: "cover" }}>
+              {/* <img src="..." className="card-img-top" alt="..."/> */}
+              <div className="card-body">
+                {/* <h5 className="card-title">Card title</h5> */}
+                <p className="card-text ser_home_card_content">4000+</p>
+              </div>
+            </div>
+            <div className="card mx-4 my-5" style={{ width: "16rem", background: `url(Images/ser_home_back.jpg)`, backgroundSize: "cover" }}>
+              {/* <img src="..." className="card-img-top" alt="..."/> */}
+              <div className="card-body">
+                {/* <h5 className="card-title">Card title</h5> */}
+                <p className="card-text ser_home_card_content">4000+</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+        <Footer />
       </div>
-    </div>
-    <div className="card mx-4 my-5" style={{width:"16rem",background:`url(Images/ser_home_back.jpg)`,backgroundSize:"cover"}}>
-      {/* <img src="..." className="card-img-top" alt="..."/> */}
-      <div className="card-body">
-        {/* <h5 className="card-title">Card title</h5> */}
-        <p className="card-text ser_home_card_content">4000+</p>
-      </div>
-    </div>
-    <div className="card mx-4 my-5" style={{width:"16rem",background:`url(Images/ser_home_back.jpg)`,backgroundSize:"cover"}}>
-      {/* <img src="..." className="card-img-top" alt="..."/> */}
-      <div className="card-body">
-        {/* <h5 className="card-title">Card title</h5> */}
-        <p className="card-text ser_home_card_content">4000+</p>
-      </div>
-    </div>
-     </div>
-     <Footer/>
-    </div>
-  
+
     </>
-   
+
   )
 }
