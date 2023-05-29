b2894f2e37a8b05261878e44c1cd2fcdb7ad883b
Mansi <mansirathod763@gmail.com>
2023-05-29 18:03:59 +0530

    app

--- we4u/src/App.js
+++ we4u/src/App.js
@@ -11,88 +11,97 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Forget } from './components/Forget';
 // import { Verify } from './components/Verify';
 import { Home } from "./components/Home";
-import { Contact } from "./components/Contact";
+import {Contact} from "./components/Contact";
 import About from "./components/About";
-import { Ser_Pro_Navbar } from "./components/Ser_Pro_Navbar";
+import {Ser_Pro_Navbar} from "./components/Ser_Pro_Navbar";
 import { Myprofile } from "./components/Myprofile";
 
-import { SpmainHome } from './components/SpmainHome';
+// import TestiMonials from './components/TestiMonials/TestiMonials';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { useState, useEffect } from "react";
+import { useState,useEffect } from "react";
 
 import Profile from "./components/Profile";
 import { ServiceProvider } from "./components/ServiceProvider";
+import { Services } from "./components/Services";
+
+
+
 import { Ser_pro_contact } from "./components/Ser_pro_contact";
 import { Sp } from "./components/Sp";
 import { Ser_add_Service } from "./components/Ser_add_Service";
 import { SubService } from "./components/SubService";
 
-
 function App() {
-  const [isLogin, setisLogin] = useState(true);
+const [isLogin, setisLogin] = useState(true);
 
-  const Authentication = () => {
-    let token = localStorage.getItem("token");
-    if (token === null) {
-      setisLogin(false);
-    }
-    console.log("token=====>", token);
-  }
+const Authentication =() =>{
+        let token = localStorage.getItem("token");
+        if(token === null){
+          setisLogin(false);
+        }
+      console.log("token=====>",token);
+}
 
-  useEffect(() => {
-    Authentication();
-  }, [isLogin])
+useEffect(() => {
+      Authentication();
+}, [isLogin])
 
 
 
   return (
     <>
-
+      
       <Router>
-        {/* <Navbar /> */}
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/register" element={<Register state={1} />}></Route>
-          <Route path="/regprof" element={<Register state={0} />}></Route>
-          <Route path="/login" element={<Login state={1} />}></Route>
-          <Route path="/loginasp" element={<Login state={0} />}></Route>
-          <Route path="/forget" element={<Forget state={1} />}></Route>
-          <Route path="/forgetasp" element={<Forget state={0} />}></Route>
-          {/* <Route path="/about" element={<about/>}></Route>  */}
+      {/* <Navbar /> */}
+      <Routes> 
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/register" element={<Register state = {1}/>}></Route>
+        <Route path="/regprof" element={<Register  state = {0}/>}></Route>
+        <Route path="/login" element={<Login state = {1} />}></Route>
+        <Route path="/loginasp" element={<Login state = {0}/>}></Route>
+        <Route path="/forget" element={<Forget state ={1}/>}></Route>
+        <Route path="/forgetasp" element={<Forget state ={0}/>}></Route>
+        {/* <Route path="/about" element={<about/>}></Route>  */}
 
-          <Route path="/logout" element={<Home />}></Route>
-          <Route path="/ser_contact" element={<Ser_pro_contact />}></Route>
-          <Route path="/logout" element={<Home />}></Route>
-          <Route path="/ser_pro_nav" element={<Ser_Pro_Navbar />}></Route>
-          <Route path="/ser_add_service" element={<Ser_add_Service />}></Route>
+        <Route path="/logout" element={<Home/>}></Route> 
+        <Route path="/logout" element={<Home/>}></Route>
+        <Route path="/ser_pro_nav" element={<Ser_Pro_Navbar/>}></Route>
+        <Route path="/ser_add_service" element={<Ser_add_Service />}></Route>
           <Route path="/service" element={<SubService />}></Route>
 
-          <Route path="/showprofile" element={<Myprofile />}></Route>
+       
+       
+        <Route path="/showprofile" element={<Myprofile/>}></Route>
 
 
-          {/* <Route path="/about" element={<TestiMonials/>}></Route> */}
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          {isLogin ? (
-            <Route path="/cart" element={<Authpage />}></Route>
-          ) : (
-            <Route path="/login" element={<Login />}></Route>
-          )}
-          {isLogin ? (
-            <Route path="/profile" element={<Profile />}></Route>
-          ) : (
-            <Route path="/login" element={<Login />}></Route>
-          )}
-          <Route path="/sphome" element={<SpmainHome />}></Route>
-          <Route path="/sphome2" element={<Sp />}></Route>
+        {/* <Route path="/about" element={<TestiMonials/>}></Route> */}
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        {isLogin ? (
+        <Route path="/cart" element={<Authpage />}></Route>
+      ) : (
+        <Route path="/login" element={<Login />}></Route>
+      )}
+        {isLogin ? (
+         <Route path="/profile" element={<Profile/>}></Route>
+      ) : (
+        <Route path="/login" element={<Login />}></Route>
+      )}
+        <Route path="/sphome" element={<ServiceProvider/>}></Route>
+
+        <Route path="/services" element={<Services/>}></Route>
+        <Route path="/sphome2" element={<Sp />}></Route>
+        {/* <Route path="/sphome2" element={<Sp />}></Route> */}
           <Route path="/contactsp" element={<Ser_pro_contact />}></Route>
 
 
-        </Routes>
+
+      </Routes>
       </Router>
     </>
   );
 }
 
 export default App;
+ 
